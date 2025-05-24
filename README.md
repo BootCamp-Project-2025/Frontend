a0e69b71adb20ee72438f1bb10b867eece2a9d34
cclaurevju <Carlos.Claure@jala.university>
2025-05-23 23:33:31 -0400

    chore: create project structure

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Frontend
+# Frontend project
+
+## Development tools
+
+1. **Styles:** tailwind
+2. **Routing:** react-router-dom
+3. **Api communication:** axios
+4. **Testing:** vitest
+
+## Setup
+
+1. Clone the repository
+2. Run the following commands
+
+```
+cd frontend
+npm install
+```
+
+## Commands
+
+```
+# deploy development
+npm run dev
+# build project
+npm run build
+# deploy built project
+npm run preview
+```
+
+## Docker development deploy
+
+```
+docker build -t <image_name>:<image_version> --no-cache -f ./dev.Dockerfile
+docker run -d -p <external_port>:5173 --name <container_name> <image_name>:<image_version>
+```
+
+## Docker production deploy
+
+```
+docker build -t <image_name>:<image_version> --no-cache -f ./prod.Dockerfile
+docker run -d -p <external_port>:80 --name <container_name> <image_name>:<image_version>
+```
---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+// eslint.config.mjs
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      'jsx-a11y': pluginJsxA11y,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'jsx-a11y/alt-text': 'warn',
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    files: ['**/*.test.{js,jsx}'],
+    plugins: {
+      vitest,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...vitest.environments['vitest/globals'].globals,
+      },
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+  },
+  {
+    files: ['**/*.json'],
+    languageOptions: {
+      parser: (await import('jsonc-eslint-parser')).default,
+    },
+    rules: {
+      'no-unused-expressions': 'off',
+    },
+  },
+]);
