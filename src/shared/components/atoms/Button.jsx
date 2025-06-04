74dfe9155651c7adfdfbcc59c271e9b2310df8f4
LucasMezzaJalaUniversity <Lucas.Mezza@jala.university>
2025-06-04 12:39:44 -0300

    refactor: set default buttons colors

--- src/shared/components/atoms/Button.jsx
+++ src/shared/components/atoms/Button.jsx
@@ -13,19 +13,20 @@ export const Button = ({
   children,
   styleType = "",
   classname = "",
-  color = "blue",
+  color = "",
   onClick = () => {},
 }) => {
   const base = "flex items-center justify-center gap-2 cursor-pointer";
 
   const variantsStyle = {
-    [BUTTON_STYLE_TYPES.ADD]: "px-4 py-2 font-bold rounded-xl text-black",
+    [BUTTON_STYLE_TYPES.ADD]:
+      "px-4 py-2 font-bold rounded-xl text-black bg-[#3b82f6]",
     [BUTTON_STYLE_TYPES.CALL_TO_ACTION]:
-      "text-white py-1 px-5 font-semibold rounded-xl",
+      "text-white py-1 px-5 font-semibold rounded-xl bg-[#3b82f6]",
     [BUTTON_STYLE_TYPES.DELETE]:
-      "bg-transparent border border-red-500 rounded-full p-2",
-    [BUTTON_STYLE_TYPES.EDIT]: "text-white rounded-full p-2",
-    [BUTTON_STYLE_TYPES.SEARCH]: "rounded-sm p-2 search",
+      "bg-transparent border border-red-500 rounded-[50%] p-2 bg-[#00000000]",
+    [BUTTON_STYLE_TYPES.EDIT]: "text-white rounded-[50%] p-2 bg-[#f6af3b]",
+    [BUTTON_STYLE_TYPES.SEARCH]: "rounded-sm p-2 search bg-[#3b82f6]",
   };
 
   const variantClasses = variantsStyle[styleType]
@@ -35,7 +36,7 @@ export const Button = ({
   return (
     <button
       className={`button ${base} ${variantClasses} ${classname}`}
-      style={{ backgroundColor: `var(--color-${color})` }}
+      style={color ? { backgroundColor: `var(--color-${color})` } : {}}
       onClick={onClick}
     >
       {children}
