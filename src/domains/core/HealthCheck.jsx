265c8e20bde3a3faae85f2b452852d4fd3d44fd8
cclaurevju <Carlos.Claure@jala.university>
2025-06-03 20:21:25 -0400

    refactor: add dependencies inside useeffect

--- src/domains/core/HealthCheck.jsx
+++ src/domains/core/HealthCheck.jsx
@@ -2,20 +2,20 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function HealthCheck() {
-  const apiUrl = import.meta.env.VITE_BASE_API_URL;
   const [healthStatus, setHealthStatus] = useState(null);
 
-  const fetchHealthStatus = async () => {
-    try {
-      const response = await axios.get(`${apiUrl}/api/health`);
-      setHealthStatus(response.data);
-    } catch (error) {
-      console.log(error);
-      setHealthStatus("API not connected ", error);
-    }
-  };
-
   useEffect(() => {
+    const apiUrl = import.meta.env.VITE_BASE_API_URL;
+    const fetchHealthStatus = async () => {
+      try {
+        const response = await axios.get(`${apiUrl}/api/health`);
+        setHealthStatus(response.data);
+      } catch (error) {
+        console.log(error);
+        setHealthStatus("API not connected ", error);
+      }
+    };
+
     fetchHealthStatus();
   }, []);
   return <div>{JSON.stringify(healthStatus)}</div>;
