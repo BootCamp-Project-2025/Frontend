952cc9b58df08bd844611c3b57e117f6bbb36662
MauricioLsalles <mauricio.leano@jala.university>
2025-06-05 14:02:57 -0400

    restore health check

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import CourseCardList from "./domains/Course/organisms/CourseCardList.jsx";
+import HealthCheck from "./domains/core/HealthCheck";
 
 export default function Router() {
   return (
@@ -8,6 +9,7 @@ export default function Router() {
       <Routes>
         <Route default path="/" element={<App />} />
         <Route path="courses" element={<CourseCardList />} />
+        <Route path="/health-check" element={<HealthCheck />} />
       </Routes>
     </BrowserRouter>
   );
