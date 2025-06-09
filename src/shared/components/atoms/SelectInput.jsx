983febb0a9dd4367e5067e355f28f09552878cc6
DanielRiosLopez <Daniel.Rios@jala.university>
2025-06-09 02:43:27 -0400

    feature: Language section added in an organism

---
+++ src/shared/components/atoms/SelectInput.jsx
@@ -0,0 +1,14 @@
+export const SelectInput = ({ register, label, options, errorMessage }) => (
+  <div className="flex flex-col">
+    <label className="font-medium">{label}</label>
+    <select {...register} className="mt-1 p-2 border rounded">
+      <option value="">Select…</option>
+      {options.map((o) => (
+        <option key={o.value} value={o.value}>
+          {o.label}
+        </option>
+      ))}
+    </select>
+    {errorMessage && <p className="text-red-500 text-sm">{errorMessage}</p>}
+  </div>
+);
