f13adb133c60f64f96463cbb634ec9138e39789a
cclaurevju <Carlos.Claure@jala.university>
2025-06-10 00:05:35 -0400

    feature: add Form and card for about me section

---
+++ src/domains/teacher/components/organisms/AboutMeForm.jsx
@@ -0,0 +1,45 @@
+import Textarea from "../../../../shared/components/atoms/Textarea";
+import { Button } from "../../../../shared/components/atoms/Button";
+import { useState } from "react";
+
+export default function AboutMeForm() {
+  const [description, setDescription] = useState();
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = () => {
+    console.log(description);
+    if (!description) setErrors([...errors, "This field is required"]);
+  };
+
+  const handleDelete = () => {
+    setDescription("");
+    handleSubmit();
+  };
+
+  const handleOnChange = (e) => {
+    setErrors([]);
+    setDescription(e.target.value);
+  };
+
+  return (
+    <div className="flex flex-col h-full justify-between">
+      <div>
+        <Textarea
+          label="Description"
+          value={description}
+          onChange={handleOnChange}
+          rows={"5"}
+          errors={errors}
+        />
+      </div>
+      <div className="flex justify-around">
+        <Button onClick={handleDelete} styleType="deleteBtn">
+          Delete
+        </Button>
+        <Button onClick={handleSubmit} styleType="addBtn">
+          Save
+        </Button>
+      </div>
+    </div>
+  );
+}
