07a9f883198fc2dd0a9721acbf3f19e4909bdef3
cclaurevju <Carlos.Claure@jala.university>
2025-06-10 00:06:40 -0400

    feat: add /about-me route to test components

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import HealthCheck from "./domains/core/HealthCheck";
+import AboutMeSection from "./domains/teacher/components/templates/AboutMeSection";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/about-me" element={<AboutMeSection />} />
         <Route path="/health-check" element={<HealthCheck />} />
       </Routes>
     </BrowserRouter>
