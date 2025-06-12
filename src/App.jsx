2d4900939aaaf83b724ce163067cbab20876adc3
Ariel Monroy <ariel.monroy@jala.university>
2025-06-12 11:08:24 -0400

    refactor(App): simplify App component by removing CertificationList

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import CertificationList from "./domains/teacher/components/organisms/CertificationList";
-
 function App() {
-  return (
-    <div className=" grid grid-cols-1 lg:grid-cols-3 gap-10 bg-gray-100 place-items-center">
-      <CertificationList />
-    </div>
-  );
+  return <>Hello world</>;
 }
 
 export default App;
