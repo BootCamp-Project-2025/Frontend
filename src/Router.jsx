695f303fc9849346cf5e3331459335b9d680a70b
MauricioLsalles <mauricio.leano@jala.university>
2025-06-15 20:54:56 -0400

    router path change

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import CourseCardList from "./domains/course/components/organisms/CourseCardList.jsx";
 import HealthCheck from "./domains/core/HealthCheck";
-import TeacherSkills from "./domains/teacher/organisms/TeacherSkills";
+import TeacherSkills from "./domains/teacher/components/organisms/TeacherSkills";
 import AboutMeSection from "./domains/teacher/components/templates/AboutMeSection";
 
 export default function Router() {
