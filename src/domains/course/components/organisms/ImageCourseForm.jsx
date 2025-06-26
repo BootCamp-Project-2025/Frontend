019dee6c98199fec45084d741334f44fb86543a4
MauricioLsalles <mauricio.leano@jala.university>
2025-06-26 15:37:35 -0400

    fix: add image title

--- src/domains/course/components/organisms/ImageCourseForm.jsx
+++ src/domains/course/components/organisms/ImageCourseForm.jsx
@@ -1,12 +1,14 @@
 import { TextInput } from "../../../../shared/components/molecules/TextInput";
 import { Button } from "../../../../shared/components/atoms/Button";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function ImageCourseForm() {
   const [image, setImage] = useState("/images/EmptyImage.svg");
-
+  const { register, setValue } = useForm();
   const handleImageChange = (event) => {
     const file = event.target.files?.[0];
+    setValue("imageName", event.target.files?.[0].name);
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
@@ -21,12 +23,7 @@ export default function ImageCourseForm() {
         style={{ minWidth: 350, minHeight: 230 }}
         className="p-4 border-1 rounded-xl flex"
       >
-        <img
-          className="mx-auto my-auto"
-          style={{ width: 100, height: 100 }}
-          src={image}
-          alt="upload image"
-        />
+        <img className="mx-auto my-auto" src={image} alt="upload image" />
       </div>
       <div>
         <p>
@@ -35,31 +32,28 @@ export default function ImageCourseForm() {
           format .jpg, .jpeg, .gif, or .png.
         </p>
         <div className="flex gap-4 py-2">
-          <div className="grow-1">
-            <TextInput placeholder="Upload file" />
+          <TextInput
+            disabled
+            id={"imageName"}
+            register={register("imageName", {})}
+            placeholder="Upload image"
+          />
+          <Button radius="small" variant="bordered" className="px-0 w-max">
             <input
+              className="hidden"
               type="file"
               accept="image/*"
-              id="upload"
-              style={{ display: "none" }}
+              id="image"
               onChange={handleImageChange}
             />
-          </div>
-          <div>
-            <Button radius="small" variant="bordered" className="w-fit px-0 ">
-              <input
-                className="hidden"
-                type="file"
-                accept="image/*"
-                id="image"
-                onChange={handleImageChange}
-              />
-              <label className="m-0 flex" htmlFor="image">
-                <span className="material-symbols-outlined">add</span>
-                <span>save image</span>
-              </label>
-            </Button>
-          </div>
+            <label
+              className="m-0 flex gap-1 hover:cursor-pointer"
+              htmlFor="image"
+            >
+              <span className="material-symbols-outlined">add</span>
+              <span className="w-max">save image</span>
+            </label>
+          </Button>
         </div>
       </div>
     </div>
