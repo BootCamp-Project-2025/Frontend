6c75e777fd1844817373a690a165e86be1df3d6d
MauricioLsalles <mauricio.leano@jala.university>
2025-07-01 13:56:06 -0400

    Refactor: new iconst added

--- src/shared/components/atoms/Icon.jsx
+++ src/shared/components/atoms/Icon.jsx
@@ -2,10 +2,12 @@ import CertificationIcon from "../../../assets/certification.svg?react";
 import EditIcon from "../../../assets/edit.svg?react";
 import EditBlackIcon from "../../../assets/editBlack.svg?react";
 import AddIcon from "../../../assets/add.svg?react";
+import PlusIcon from "../../../assets/plus.svg?react";
 import CloseIcon from "../../../assets/close.svg?react";
 import SyllabusAlertIcon from "../../../assets/syllabusAlert.svg?react";
 import TrashCanIcon from "../../../assets/trashCan.svg?react";
-import VectorPointerIcon from "../../../assets/vectorPointer.svg?react";
+import VectorUpIcon from "../../../assets/vectorPointer.svg?react";
+import VectorDownIcon from "../../../assets/vectorDown.svg?react";
 import PropTypes from "prop-types";
 
 const icons = {
@@ -13,10 +15,12 @@ const icons = {
   edit: EditIcon,
   editBlack: EditBlackIcon,
   add: AddIcon,
+  plus: PlusIcon,
   close: CloseIcon,
   syllabusAlert: SyllabusAlertIcon,
   trashCan: TrashCanIcon,
-  vectorPointer: VectorPointerIcon,
+  vectorUp: VectorUpIcon,
+  vectorDown: VectorDownIcon,
 };
 
 export function Icon({ icon, className }) {
