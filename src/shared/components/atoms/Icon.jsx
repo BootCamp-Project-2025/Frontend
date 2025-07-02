284d7efdb91dcb462e882e75c485a9e03afcabe3
MauricioLsalles <mauricio.leano@jala.university>
2025-07-02 09:04:50 -0400

    refactor: added new icon

--- src/shared/components/atoms/Icon.jsx
+++ src/shared/components/atoms/Icon.jsx
@@ -8,6 +8,7 @@ import SyllabusAlertIcon from "../../../assets/syllabusAlert.svg?react";
 import TrashCanIcon from "../../../assets/trashCan.svg?react";
 import VectorUpIcon from "../../../assets/vectorPointer.svg?react";
 import VectorDownIcon from "../../../assets/vectorDown.svg?react";
+import SaveIcon from "../../../assets/save.svg?react";
 import PropTypes from "prop-types";
 
 const icons = {
@@ -15,6 +16,7 @@ const icons = {
   edit: EditIcon,
   editBlack: EditBlackIcon,
   add: AddIcon,
+  save: SaveIcon,
   plus: PlusIcon,
   close: CloseIcon,
   syllabusAlert: SyllabusAlertIcon,
