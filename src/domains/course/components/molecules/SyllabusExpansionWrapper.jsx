fdd69e859e9bc40c915d0f3a36c358478c7e5550
MauricioLsalles <mauricio.leano@jala.university>
2025-07-02 22:26:17 -0400

    rename

--- src/domains/course/components/molecules/SyllabusExpansionWrapper.jsx
+++ src/domains/course/components/molecules/SyllabusExpansionWrapper.jsx
@@ -2,23 +2,29 @@ import PropTypes from "prop-types";
 import { Title } from "../../../../shared/components/atoms/Title";
 import { Icon } from "../../../../shared/components/atoms/Icon";
 import { Button } from "../../../../shared/components/atoms/Button";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export default function ExpansionWrapper({
-  sectionTitle = "",
+export default function SyllabusExpansionWrapper({
   saveTitle,
   save,
-  enableSave = false,
   erase,
+  enableSave = false,
+  newSection = false,
+  borderTitle = true,
+  sectionTitle = "",
   title = "",
   children,
   className = "",
-  borderTitle = true,
   ...props
 }) {
   const [displayChild, setDisplayChild] = useState(true);
   const [editTitle, setEditTitle] = useState(false);
   const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, [editTitle]);
+
   function enableEdit() {
     if (!editTitle) {
       inputRef.current.value = title;
@@ -28,7 +34,7 @@ export default function ExpansionWrapper({
   return (
     <div className={`py-2 ${className}`}>
       <div
-        className={`flex items-center justify-between my-2 ${borderTitle ? "border-b-1" : ""} `}
+        className={`flex items-center justify-between my-2 py-1 ${borderTitle ? "border-b-1" : ""} `}
         {...props}
       >
         <div className="flex items-center gap-4">
@@ -44,6 +50,9 @@ export default function ExpansionWrapper({
           >
             {title}
           </span>
+          <span className={`${newSection ? "" : "hidden"} text-blue-500`}>
+            new
+          </span>
 
           {editTitle ? (
             <Button
@@ -54,7 +63,7 @@ export default function ExpansionWrapper({
               variant="light"
               color="secondary"
             >
-              <Icon className={"w-4 h-4"} icon={"save"} />
+              <Icon className={"fill-black w-4 h-4"} icon={"save"} />
             </Button>
           ) : (
             <Button onClick={enableEdit} variant="light" color="secondary">
@@ -65,11 +74,10 @@ export default function ExpansionWrapper({
         <div>
           <Button
             onClick={save}
-            variant="light"
             color="secondary"
-            className={`px-5 border-2 border-blue-500 ${enableSave ? "" : "hidden"}`}
+            className={`px-5 bg-blue-500 ${enableSave ? "" : "hidden"}`}
           >
-            <Icon className={`w-4 h-4`} icon={"save"} />
+            <Icon className={`fill-white w-4 h-4`} icon={"save"} />
           </Button>
           <Button
             onClick={erase}
@@ -99,14 +107,15 @@ export default function ExpansionWrapper({
   );
 }
 
-ExpansionWrapper.propTypes = {
+SyllabusExpansionWrapper.propTypes = {
   title: PropTypes.string,
   className: PropTypes.string,
   sectionTitle: PropTypes.string,
   children: PropTypes.element,
-  borderTitle: PropTypes.bool,
-  enableSave: PropTypes.bool,
   save: PropTypes.func,
   saveTitle: PropTypes.func,
   erase: PropTypes.func,
+  borderTitle: PropTypes.bool,
+  enableSave: PropTypes.bool,
+  newSection: PropTypes.bool,
 };
