d33f5f5ea4fefc8b45ce8dfcc378e7ae30ccac16
MauricioLsalles <mauricio.leano@jala.university>
2025-07-07 00:39:44 -0400

    feat: temporary fetch functions

---
+++ src/domains/course/api/ApiGet.js
@@ -0,0 +1,18 @@
+import { learningAPI } from "../axios/AxiosCourseConnection";
+
+export const ApiGet = async (path, body) => {
+  let error = false;
+  let status;
+  let responseData = null;
+  try {
+    const response = await learningAPI.get(path, body);
+    status = response.statusCode;
+    responseData = response.data;
+    status = response.status;
+  } catch (err) {
+    error = true;
+    status = err.status;
+    responseData = err.response.data ?? null;
+  }
+  return { responseData, error, status };
+};
