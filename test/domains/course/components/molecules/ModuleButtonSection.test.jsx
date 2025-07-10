7daad31dbcd4418ca03841f616e8f1b845976624
MauricioLsalles <mauricio.leano@jala.university>
2025-07-10 11:20:06 -0400

    feat: created test

---
+++ test/domains/course/components/molecules/ModuleButtonSection.test.jsx
@@ -0,0 +1,36 @@
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { render, screen } from "@testing-library/react";
+import ButtonSection from "../../../../../src/domains/course/components/molecules/ModuleButtonSection";
+
+describe("EraseConfirmation test component", () => {
+  const click1 = vi.fn();
+  const click2 = vi.fn();
+
+  beforeEach(() => {
+    click1.mockClear();
+    click2.mockClear();
+  });
+  it("renders correctly", () => {
+    const buttonProps = [{ text: "testText", onClick: () => click1() }];
+    render(<ButtonSection buttonProps={buttonProps} />);
+    const button = screen.getByText("testText");
+    button.click();
+    expect(button).toBeInTheDocument();
+    expect(click1).toHaveBeenCalled();
+  });
+  it("renders multiple buttons correctly", () => {
+    const buttonProps = [
+      { text: "testText", onClick: () => click1() },
+      { text: "testText2", onClick: () => click2() },
+    ];
+    render(<ButtonSection buttonProps={buttonProps} />);
+    const button = screen.getByText("testText");
+    const button2 = screen.getByText("testText2");
+    button.click();
+    button2.click();
+    expect(button).toBeInTheDocument();
+    expect(button2).toBeInTheDocument();
+    expect(click1).toHaveBeenCalled();
+    expect(click2).toHaveBeenCalled();
+  });
+});
