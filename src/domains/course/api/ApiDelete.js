902c75a42570c374638c840fb41f24167fa0109d
MauricioLsalles <mauricio.leano@jala.university>
2025-07-10 10:10:09 -0400

    refactor: updated how fetch handles data

--- src/domains/course/api/ApiDelete.js
+++ src/domains/course/api/ApiDelete.js
@@ -3,13 +3,16 @@ import { learningAPI } from "../axios/AxiosCourseConnection";
 export const ApiDelete = async (path) => {
   let error = false;
   let status;
+  let data;
   try {
     const response = await learningAPI.delete(path);
     status = response.statusCode;
     status = response.status;
+    data = response.data.data;
   } catch (err) {
     error = true;
     status = err.status;
+    data = err.response.data ?? err;
   }
-  return { error, status };
+  return { data, error, status };
 };
