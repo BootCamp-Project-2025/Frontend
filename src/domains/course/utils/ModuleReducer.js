d0595eb864a0c707e8abf4ab30e4caba3db83cbb
MauricioLsalles <mauricio.leano@jala.university>
2025-07-10 18:22:49 -0400

    refactor: ModuleReducer uses classes of Module and Lessons and its methods instead of objects

--- src/domains/course/utils/ModuleReducer.js
+++ src/domains/course/utils/ModuleReducer.js
@@ -70,43 +70,57 @@ export const moduleReducer = (state, action) => {
       const newState = [...state];
       const module = newState[action.modulePosition].clone();
       const lesson = module.lessons[action.lessonPosition].clone();
-      lesson.deleteVideo(action.url);
+      lesson.deleteVideoUrls(action.url);
       module.lessons[action.lessonPosition] = lesson;
       newState[action.modulePosition] = module;
       return newState;
     }
     case "EDIT_MODULE_TITLE": {
       const newState = [...state];
-      if (newState[action.modulePosition].title !== action.title)
-        newState[action.modulePosition].edited = true;
-      newState[action.modulePosition].title = action.title;
+      const module = newState[action.modulePosition].clone();
+      module.setTitle(action.title);
+      newState[action.modulePosition] = module;
       return newState;
     }
     case "EDIT_LESSON_TITLE": {
       const newState = [...state];
-      const module = { ...newState[action.modulePosition] };
-      const lessons = [...module.lessons];
-      const lesson = { ...lessons[action.lessonPosition] };
-      if (lesson.title !== action.title) lesson.edited = true;
-      lesson.title = action.title;
-      lessons[action.lessonPosition] = lesson;
-      module.lessons = lessons;
+      const module = newState[action.modulePosition].clone();
+      const lesson = module.lessons[action.lessonPosition].clone();
+      lesson.setTitle(action.title);
+      module.lessons[action.lessonPosition] = lesson;
       newState[action.modulePosition] = module;
       return newState;
     }
     case "EDIT_LESSON_DESCRIPTION": {
       const newState = [...state];
-      const module = { ...newState[action.modulePosition] };
-      const lessons = [...module.lessons];
-      const lesson = { ...lessons[action.lessonPosition] };
-      lesson.description = action.description;
-      lessons[action.lessonPosition] = lesson;
-      module.lessons = lessons;
+      const module = newState[action.modulePosition].clone();
+      const lesson = module.lessons[action.lessonPosition].clone();
+      lesson.setDescription(action.description);
+      module.lessons[action.lessonPosition] = lesson;
       newState[action.modulePosition] = module;
       return newState;
     }
+
     case "SET":
-      return action.payload;
+      return action.payload.map((module) => {
+        return Module.builder()
+          .id(module.id)
+          .title(module.title)
+          .lessons(
+            module.lessons.map((lesson) => {
+              return Lesson.builder()
+                .id(lesson.id)
+                .description(lesson.description)
+                .title(lesson.title)
+                .resources(lesson.resources)
+                .videoUrls(lesson.videoUrls)
+                .position(lesson.position)
+                .build();
+            })
+          )
+          .position(module.position)
+          .build();
+      });
     case "DELETE_MODULE": {
       const newState = [...state];
       return newState.filter(
@@ -114,35 +128,25 @@ export const moduleReducer = (state, action) => {
       );
     }
     case "DELETE_LESSON": {
-      console.log(action.lessonPosition);
       const newState = [...state];
-      const module = { ...newState[action.modulePosition] };
-      const lessons = [...module.lessons];
-      module.lessons = lessons.filter(
-        (lesson) => lesson.position !== action.lessonPosition
-      );
+      const module = newState[action.modulePosition].clone();
+      module.deleteLesson(action.lessonPosition);
       newState[action.modulePosition] = module;
       return newState;
     }
     case "SAVE_LESSON": {
       const newState = [...state];
-      const module = { ...newState[action.modulePosition] };
-      const lessons = [...module.lessons];
-      const lesson = { ...lessons[action.lessonPosition] };
-      lesson.edited = false;
-      lesson.new = false;
-      lesson.id = action.id;
-      lessons[action.lessonPosition] = lesson;
-      module.lessons = lessons;
+      const module = newState[action.modulePosition].clone();
+      const lesson = module.lessons[action.lessonPosition].clone();
+      lesson.save(action.id);
+      module.lessons[action.lessonPosition] = lesson;
       newState[action.modulePosition] = module;
       return newState;
     }
     case "SAVE_MODULE": {
       const newState = [...state];
-      const module = { ...newState[action.modulePosition] };
-      module.edited = false;
-      module.new = false;
-      module.id = action.id;
+      const module = newState[action.modulePosition].clone();
+      module.save(action.id);
       newState[action.modulePosition] = module;
       return newState;
     }
