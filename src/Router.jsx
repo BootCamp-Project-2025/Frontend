6c910d4ef1f386e694500aab64ebf50bb39a3a2c
MauricioLsalles <mauricio.leano@jala.university>
2025-07-10 10:06:43 -0400

    feat: integrated syllabus

--- src/Router.jsx
+++ src/Router.jsx
@@ -41,7 +41,7 @@ export default function Router() {
           />
           <Route
             path="/dashboard/courses/:courseId/syllabus"
-            element={<main>Edit syllabus page</main>}
+            element={<CourseSyllabus />}
           />
           <Route path="/dashboard/chats" element={<main>Chats</main>} />
         </Route>
@@ -89,7 +89,6 @@ export default function Router() {
           <Route path="dropdown-gallery" element={<DropdownSelectGallery />} />
           <Route path="file-upload" element={<FileUploadExample />} />
           <Route path="texteditor-showcase" element={<TextEditorShowcase />} />
-          <Route path="course-syllabus" element={<CourseSyllabus />} />
 
           <Route
             path="*"
