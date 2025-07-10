160d12d85abf65635bc21826ddca1df08ee573d0
MauricioLsalles <mauricio.leano@jala.university>
2025-07-10 16:39:45 -0400

    fix: added return this to the lesson builder

--- src/domains/course/classes/Lesson.js
+++ src/domains/course/classes/Lesson.js
@@ -19,23 +19,30 @@ class LessonBuilder {
   }
   title(title) {
     this.lesson.title = title;
+    return this;
   }
   resources(resources) {
     this.lesson.resources = resources;
+    return this;
   }
   videoUrls(videoUrls) {
     this.lesson.videoUrls = videoUrls;
+    return this;
   }
   position(position) {
     this.lesson.position = position;
+    return this;
   }
   description(description) {
     this.lesson.description = description;
+    return this;
   }
   isNew(isNew) {
     this.lesson.isNew = isNew;
+    return this;
   }
   isEdited(isEdited) {
     this.lesson.isEdited = isEdited;
+    return this;
   }
 }
