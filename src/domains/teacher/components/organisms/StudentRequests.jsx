fa05dc5d90ac7b9d4a0ea03de9797e9949151fd1
MauricioLsalles <mauricio.leano@jala.university>
2025-07-15 21:43:25 -0400

    feat: Student request list layout completed

---
+++ src/domains/teacher/components/organisms/StudentRequests.jsx
@@ -0,0 +1,42 @@
+import SearchBar from "../molecules/SearchBar";
+import StudentEmptyRequestsMessage from "../molecules/StudentEmptyRequestsMessage";
+import RequestList from "./RequestList";
+
+export default function StudentRequests() {
+  const requestList = [
+    {
+      title: "Request Title",
+      description:
+        "I require to learn JavaScript over the next month. My primary goals are to understand key concepts such as closures, hoisting, scope, this keyword, promises, async/await, and event loop. Additionally, I want to become comfortable working with arrays and objects, using higher-order functions, and writing clean, modular code. This foundation will help me build more complex applications and prepare me for frameworks like React or Node.js in the near future.",
+      estimation: 5,
+    },
+    {
+      title: "Request Title",
+      description:
+        "I require to learn JavaScript over the next month. My primary goals are to understand key concepts such as closures, hoisting, scope, this keyword, promises, async/await, and event loop. Additionally, I want to become comfortable working with arrays and objects, using higher-order functions, and writing clean, modular code. This foundation will help me build more complex applications and prepare me for frameworks like React or Node.js in the near future.",
+      estimation: 5,
+    },
+    {
+      title: "Request Title",
+      description:
+        "I require to learn JavaScript over the next month. My primary goals are to understand key concepts such as closures, hoisting, scope, this keyword, promises, async/await, and event loop. Additionally, I want to become comfortable working with arrays and objects, using higher-order functions, and writing clean, modular code. This foundation will help me build more complex applications and prepare me for frameworks like React or Node.js in the near future.",
+      estimation: 5,
+    },
+    {
+      title: "Request Title",
+      description:
+        "I require to learn JavaScript over the next month. My primary goals are to understand key concepts such as closures, hoisting, scope, this keyword, promises, async/await, and event loop. Additionally, I want to become comfortable working with arrays and objects, using higher-order functions, and writing clean, modular code. This foundation will help me build more complex applications and prepare me for frameworks like React or Node.js in the near future.",
+      estimation: 5,
+    },
+  ];
+  return (
+    <div>
+      <SearchBar placeHolder="Find a specific request" />
+      {requestList.length === 0 ? (
+        <StudentEmptyRequestsMessage />
+      ) : (
+        <RequestList requestList={requestList} />
+      )}
+    </div>
+  );
+}
