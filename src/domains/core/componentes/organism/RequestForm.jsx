62172ef15777db2af7c0bb0e700d23d7f2422789
MauricioLsalles <mauricio.leano@jala.university>
2025-07-16 15:25:01 -0400

    feat: added pop up functionallity to Student request

--- src/domains/core/componentes/organism/RequestForm.jsx
+++ src/domains/core/componentes/organism/RequestForm.jsx
@@ -1,3 +1,9 @@
-export default function RequestForm() {
-  return <div>RequestForm</div>;
+import PropTypes from "prop-types";
+
+export default function RequestForm({ closePopup }) {
+  return <button onClick={closePopup}>close</button>;
 }
+
+RequestForm.propTypes = {
+  closePopup: PropTypes.func,
+};
--- src/domains/core/componentes/organism/StudentRequests.jsx
+++ src/domains/core/componentes/organism/StudentRequests.jsx
@@ -1,41 +1,54 @@
+import { useCallback } from "react";
 import SearchBar from "../../../../shared/components/molecules/SearchBar";
+import usePopup from "../../../../shared/hooks/usePopup";
 import StudentEmptyRequestsMessage from "../molecules/StudentEmptyRequestsMessage";
 import RequestList from "./RequestList";
+import RequestForm from "./RequestForm";
+
+const requestList = [
+  {
+    title: "Request Title",
+    description:
+      "I require to learn JavaScript over the next month. My primary goals are to understand key concepts such as closures, hoisting, scope, this keyword, promises, async/await, and event loop. Additionally, I want to become comfortable working with arrays and objects, using higher-order functions, and writing clean, modular code. This foundation will help me build more complex applications and prepare me for frameworks like React or Node.js in the near future.",
+    estimation: 5,
+  },
+  {
+    title: "Request Title",
+    description:
+      "I require to learn JavaScript over the next month. My primary goals are to understand key concepts such as closures, hoisting, scope, this keyword, promises, async/await, and event loop. Additionally, I want to become comfortable working with arrays and objects, using higher-order functions, and writing clean, modular code. This foundation will help me build more complex applications and prepare me for frameworks like React or Node.js in the near future.",
+    estimation: 5,
+  },
+  {
+    title: "Request Title",
+    description:
+      "I require to learn JavaScript over the next month. My primary goals are to understand key concepts such as closures, hoisting, scope, this keyword, promises, async/await, and event loop. Additionally, I want to become comfortable working with arrays and objects, using higher-order functions, and writing clean, modular code. This foundation will help me build more complex applications and prepare me for frameworks like React or Node.js in the near future.",
+    estimation: 5,
+  },
+  {
+    title: "Request Title",
+    description:
+      "I require to learn JavaScript over the next month. My primary goals are to understand key concepts such as closures, hoisting, scope, this keyword, promises, async/await, and event loop. Additionally, I want to become comfortable working with arrays and objects, using higher-order functions, and writing clean, modular code. This foundation will help me build more complex applications and prepare me for frameworks like React or Node.js in the near future.",
+    estimation: 5,
+  },
+];
 
 export default function StudentRequests() {
-  const requestList = [
-    {
-      title: "Request Title",
-      description:
-        "I require to learn JavaScript over the next month. My primary goals are to understand key concepts such as closures, hoisting, scope, this keyword, promises, async/await, and event loop. Additionally, I want to become comfortable working with arrays and objects, using higher-order functions, and writing clean, modular code. This foundation will help me build more complex applications and prepare me for frameworks like React or Node.js in the near future.",
-      estimation: 5,
-    },
-    {
-      title: "Request Title",
-      description:
-        "I require to learn JavaScript over the next month. My primary goals are to understand key concepts such as closures, hoisting, scope, this keyword, promises, async/await, and event loop. Additionally, I want to become comfortable working with arrays and objects, using higher-order functions, and writing clean, modular code. This foundation will help me build more complex applications and prepare me for frameworks like React or Node.js in the near future.",
-      estimation: 5,
-    },
-    {
-      title: "Request Title",
-      description:
-        "I require to learn JavaScript over the next month. My primary goals are to understand key concepts such as closures, hoisting, scope, this keyword, promises, async/await, and event loop. Additionally, I want to become comfortable working with arrays and objects, using higher-order functions, and writing clean, modular code. This foundation will help me build more complex applications and prepare me for frameworks like React or Node.js in the near future.",
-      estimation: 5,
-    },
-    {
-      title: "Request Title",
-      description:
-        "I require to learn JavaScript over the next month. My primary goals are to understand key concepts such as closures, hoisting, scope, this keyword, promises, async/await, and event loop. Additionally, I want to become comfortable working with arrays and objects, using higher-order functions, and writing clean, modular code. This foundation will help me build more complex applications and prepare me for frameworks like React or Node.js in the near future.",
-      estimation: 5,
-    },
-  ];
+  const { openPopup, closePopup } = usePopup();
+
+  const handleNewRequest = useCallback(() => {
+    openPopup(RequestForm, { closePopup: closePopup }, true);
+  }, [openPopup, closePopup]);
+
   return (
     <div>
-      <SearchBar placeHolder="Find a specific request" />
+      <SearchBar placeholder="Find a specific request" />
       {requestList.length === 0 ? (
-        <StudentEmptyRequestsMessage />
+        <StudentEmptyRequestsMessage handleNewRequest={handleNewRequest} />
       ) : (
-        <RequestList requestList={requestList} />
+        <RequestList
+          handleNewRequest={handleNewRequest}
+          requestList={requestList}
+        />
       )}
     </div>
   );
