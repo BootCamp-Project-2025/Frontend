a4a7a9af9a76c4b763f47dffbbd6acaa6e0da92b
MauricioLsalles <mauricio.leano@jala.university>
2025-07-15 21:43:25 -0400

    feat: Student request list layout completed

---
+++ src/domains/teacher/components/organisms/RequestList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import RequestCard from "./RequestCard";
+import { Button } from "../../../../shared/components/atoms/Button";
+
+export default function RequestList({ requestList }) {
+  return (
+    <div className=" mt-10 flex flex-col">
+      <Button variant="bordered" className={"self-center"}>
+        Create a new request
+      </Button>
+      {requestList.map((request, id) => (
+        <RequestCard key={id} request={request} />
+      ))}
+    </div>
+  );
+}
+
+RequestList.propTypes = {
+  requestList: PropTypes.array.isRequired,
+};
