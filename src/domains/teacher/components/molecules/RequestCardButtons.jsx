af728582cbde602742dd05a3fcdb0f5b975777f2
MauricioLsalles <mauricio.leano@jala.university>
2025-07-15 22:50:45 -0400

    feat: added tests to RequestCardButton

--- src/domains/teacher/components/molecules/RequestCardButtons.jsx
+++ src/domains/teacher/components/molecules/RequestCardButtons.jsx
@@ -1,20 +1,44 @@
 import { Button } from "../../../../shared/components/atoms/Button";
+import PropTypes from "prop-types";
 
-export default function RequestCardButtons({ ...props }) {
+export default function RequestCardButtons({
+  openRequest,
+  editRequest,
+  deleteRequest,
+  ...props
+}) {
   return (
     <div {...props}>
       <Button
+        onClick={openRequest}
+        data-testid="openButton"
         contentClassName={"m-auto w-fit whitespace-nowrap"}
         variant="bordered"
       >
         Open request
       </Button>
-      <Button contentClassName={"m-auto w-fit"} variant="bordered">
+      <Button
+        onClick={editRequest}
+        data-testid="editButton"
+        contentClassName={"m-auto w-fit"}
+        variant="bordered"
+      >
         Edit
       </Button>
-      <Button contentClassName={"m-auto w-fit"} variant="bordered">
+      <Button
+        onClick={deleteRequest}
+        data-testid="deleteButton"
+        contentClassName={"m-auto w-fit"}
+        variant="bordered"
+      >
         Delete
       </Button>
     </div>
   );
 }
+
+RequestCardButtons.propTypes = {
+  openRequest: PropTypes.func.isRequired,
+  editRequest: PropTypes.func.isRequired,
+  deleteRequest: PropTypes.func.isRequired,
+};
