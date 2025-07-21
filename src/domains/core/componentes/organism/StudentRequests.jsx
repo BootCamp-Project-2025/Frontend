9b4abdb8694ed6acc58becf9b3fc72505f0f418b
MauricioLsalles <mauricio.leano@jala.university>
2025-07-21 14:21:37 -0400

    Revert "fix: tailwind correction to mach rest of the project"
    This reverts commit e141118f6cac498c11d647b23f7d9ac95786c7cc.

--- src/domains/core/componentes/organism/StudentRequests.jsx
+++ src/domains/core/componentes/organism/StudentRequests.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import SearchBar from "../../../../shared/components/molecules/SearchBar";
 import usePopup from "../../../../shared/hooks/usePopup";
 import StudentEmptyRequestsMessage from "../molecules/StudentEmptyRequestsMessage";
@@ -8,12 +8,11 @@ import { getRequest } from "../../../../shared/api/getRequest";
 import { postRequest } from "../../../../shared/api/postRequest";
 import { useToastContext } from "../../../../shared/contexts/ToastContext";
 import { deleteRequest } from "../../../../shared/api/deleteRequest";
-import DeleteCardPopup from "../../../teacher/components/atoms/DeleteCardPopup";
 
 export default function StudentRequests() {
-  const { showToast } = useToastContext();
   const { openPopup, closePopup } = usePopup();
   const [requestList, setRequestList] = useState([]);
+  const { showToast } = useToastContext();
 
   const loadData = useCallback(async () => {
     const response = await getRequest("/requests/validUserRequests");
@@ -25,6 +24,23 @@ export default function StudentRequests() {
   useEffect(() => {
     loadData();
   }, [loadData]);
+
+  const saveRequest = useContext(async (data) => {
+    const response = await postRequest("/requests", {
+      ...data,
+      status: "AVAILABLE",
+    });
+    manageToast(response.success);
+    return response;
+  }, []);
+
+  const deleteUserRequest = useCallback(
+    async (requestId) => {
+      const response = await deleteRequest(`/requests/${requestId}`);
+      manageToast(response.success);
+    },
+    [manageToast]
+  );
 
   const manageToast = useCallback(
     (success) => {
@@ -38,42 +54,6 @@ export default function StudentRequests() {
     [loadData, showToast]
   );
 
-  const saveRequest = useCallback(
-    async (data) => {
-      const response = await postRequest("/requests", {
-        ...data,
-        status: "AVAILABLE",
-      });
-      manageToast(response.success);
-      return response;
-    },
-    [manageToast]
-  );
-
-  const deleteUserRequest = useCallback(
-    async (requestId) => {
-      const response = await deleteRequest(`/requests/${requestId}`);
-      manageToast(response.success);
-    },
-    [manageToast]
-  );
-
-  const handleDeleteRequestPopUp = useCallback(
-    (requestId) => {
-      openPopup(
-        DeleteCardPopup,
-        {
-          title: "Delete Card",
-          msg: "This action is irreversible. Please confirm to proceed.",
-          closePopup,
-          deleteAction: () => deleteUserRequest(requestId),
-        },
-        false
-      );
-    },
-    [openPopup, closePopup, deleteUserRequest]
-  );
-
   const handleCreateRequest = useCallback(() => {
     openPopup(
       RequestForm,
@@ -83,7 +63,7 @@ export default function StudentRequests() {
   }, [openPopup, saveRequest, closePopup]);
 
   return (
-    <div className="max-w-[90rem] w-full px-8 py-4 mx-auto">
+    <div>
       <SearchBar placeholder="Find a specific request" />
       {requestList.length === 0 ? (
         <StudentEmptyRequestsMessage
@@ -91,7 +71,7 @@ export default function StudentRequests() {
         />
       ) : (
         <RequestList
-          deleteRequest={handleDeleteRequestPopUp}
+          deleteRequest={deleteUserRequest}
           handleCreateRequest={handleCreateRequest}
           requestList={requestList}
         />
