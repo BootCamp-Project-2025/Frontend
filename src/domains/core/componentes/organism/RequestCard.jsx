fa04abe3d3e54110ac758109aa5550bcc5b7b6ee
MauricioLsalles <mauricio.leano@jala.university>
2025-07-21 11:16:53 -0400

    refactor: added delete function to request card

--- src/domains/core/componentes/organism/RequestCard.jsx
+++ src/domains/core/componentes/organism/RequestCard.jsx
@@ -2,23 +2,28 @@ import PropTypes from "prop-types";
 import RequestCardInfo from "../molecules/RequestCardInfo";
 import RequestCardButtons from "../molecules/RequestCardButtons";
 
-export default function RequestCard({ request }) {
+export default function RequestCard({ deleteRequest, request }) {
   return (
-    <div className="flex gap-6 border-1 border-gray-300 p-6 shadow-[0_4px_4px_rgba(0,0,0,0.30)] my-6">
+    <div className="flex justify-between gap-6 border-1 border-gray-300 p-6 shadow-[0_4px_4px_rgba(0,0,0,0.30)] my-6">
       <RequestCardInfo
         className={"flex flex-col gap-2"}
         title={request.title}
         description={request.description}
         details={`Time estimation: ${request.estimation} hours`}
       />
-      <RequestCardButtons className={"flex flex-col gap-2"} />
+      <RequestCardButtons
+        deleteRequest={() => deleteRequest(request.id)}
+        className={"flex flex-col gap-2"}
+      />
     </div>
   );
 }
 
 RequestCard.propTypes = {
+  deleteRequest: PropTypes.func.isRequired,
   request: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     estimation: PropTypes.number.isRequired,
   }).isRequired,
