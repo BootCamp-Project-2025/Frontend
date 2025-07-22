c31f0d388ccb011edc14ab7909e67d97eb6f03aa
MauricioLsalles <mauricio.leano@jala.university>
2025-07-22 10:56:26 -0400

    refactor: added search function to searchBar

--- src/shared/components/molecules/SearchBar.jsx
+++ src/shared/components/molecules/SearchBar.jsx
@@ -1,7 +1,7 @@
 import { Icon } from "../atoms/Icon";
 import PropTypes from "prop-types";
 
-export default function SearchBar({ className, ...props }) {
+export default function SearchBar({ className, seach, ...props }) {
   return (
     <div className={`flex border-1 border-gray-400 rounded-sm ${className}`}>
       <input
@@ -9,7 +9,10 @@ export default function SearchBar({ className, ...props }) {
         {...props}
         className="w-full py-2 px-3 focus:outline-gray-400"
       />
-      <button className="px-2 border-l-1 border-gray-400 hover:cursor-pointer">
+      <button
+        onClick={seach}
+        className="px-2 border-l-1 border-gray-400 hover:cursor-pointer"
+      >
         <Icon icon={"search"} />
       </button>
     </div>
@@ -18,4 +21,5 @@ export default function SearchBar({ className, ...props }) {
 
 SearchBar.propTypes = {
   className: PropTypes.string,
+  seach: PropTypes.func,
 };
