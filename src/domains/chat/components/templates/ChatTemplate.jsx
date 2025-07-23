9fffcd060fccf6a5f7b09e4a611204e015255a30
cclaurevju <Carlos.Claure@jala.university>
2025-07-23 08:56:26 -0400

    feat: create ChatTemplate component

--- src/domains/chat/components/templates/ChatTemplate.jsx
+++ src/domains/chat/components/templates/ChatTemplate.jsx
@@ -1,85 +1,46 @@
-import { useEffect, useReducer } from "react";
+import { useEffect } from "react";
 import ChatInput from "../molecules/ChatInput";
 import { ChatHeader } from "../organisms/ChatHeader";
 import { ChatMessageList } from "../organisms/ChatMessageList";
-import { v4 as uuid } from "uuid";
+import PropTypes from "prop-types";
+import { useNavigate, useParams } from "react-router-dom";
+import { useChat } from "../../hooks/useChat";
+import { useAuth } from "../../../../shared/hooks/useAuth";
 
-const userId = "fdb70cf2-5d37-4609-85b6-42607da1d405";
-// const userId = "83303737-8ee5-4ba2-b6f4-f19c3f596817";
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "fetch-data":
-      return action.data;
-    case "add-message":
-      console.log(action.message);
-      return { ...state, messages: [...state.messages, action.message] };
-    case "update-message-status":
-      console.log(action.message);
-      return {
-        ...state,
-        messages: [
-          ...state.messages.map((message) => {
-            if (message.id == action.message.id) return action.message;
-            return message;
-          }),
-        ],
-      };
+export default function ChatTemplate({ chatIdProp = null }) {
+  // const [chatReducer, dispatch] = useReducer(reducer);
+  const params = useParams();
+  const navigate = useNavigate();
+  const { isAuthenticated, user } = useAuth();
+  const { userId, chat, setUserId, joinChat, sendMessage, leaveChat } =
+    useChat();
+  const chatId = chatIdProp ?? params.chatId;
 
-    default:
-      break;
-  }
-};
-
-export default function ChatTemplate() {
-  const [chat, dispatch] = useReducer(reducer);
-  const fetchJSON = async (url) => {
-    const res = await fetch(url);
-    if (!res.ok) throw new Error(`Fetch failed (${url})`);
-    return res.json();
-  };
-
-  const fetchData = () => fetchJSON("/chatMock/chat.json");
   useEffect(() => {
-    fetchData().then((data) => dispatch({ type: "fetch-data", data }));
-  }, []);
-
-  const buildMessage = (content, type) => {
-    return {
-      id: uuid(),
-      content: content,
-      type: type,
-      timestamp: new Date().toISOString(),
-      senderId: chat.participantsIds[0], //UserId
-      status: "SENT",
-      chatId: chat.id,
+    if (isAuthenticated) {
+      setUserId(user.id);
+      joinChat(chatId, user.id);
+    }
+    if (isAuthenticated && chat) {
+      if (!chat.participantsIds.includes(userId)) navigate("..");
+    }
+    return () => {
+      leaveChat(chatId);
     };
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated, chatIdProp]);
 
   const handleSendMessage = (content, type) => {
-    const message = buildMessage(content, type);
-    dispatch({ type: "add-message", message });
-    //TODO throw send-message action to socket
-    //TODO instead of setTimeout listen to the socket response with callback and update the message status
-    setTimeout(() => {
-      dispatch({
-        type: "update-message-status",
-        message: { ...message, status: "DELIVERED" },
-      });
-      setTimeout(() => {
-        dispatch({
-          type: "update-message-status",
-          message: { ...message, status: "READ" },
-        });
-      }, 2000);
-    }, 2000);
+    sendMessage(content, type);
   };
   return (
-    // h-[calc(100vh-<header heigh>)], hide footer
-    <div className="h-[calc(100vh-5.625rem)] flex flex-col">
+    // Container height to fill screen must be: h-[calc(100vh-<header heigh>)], hide footer
+    <div className="h-full flex flex-col overflow-y-hidden">
       {chat ? (
         <>
           <ChatHeader
             participantsIds={chat.participantsIds.filter((id) => id != userId)}
+            chatName={chat.name}
           />
           <ChatMessageList ownerId={userId} messages={chat.messages} />
           <ChatInput handleSubmit={handleSendMessage} />
@@ -90,3 +51,7 @@ export default function ChatTemplate() {
     </div>
   );
 }
+
+ChatTemplate.propTypes = {
+  chatIdProp: PropTypes.string,
+};
