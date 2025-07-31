d7ebbce3f4ba4d3be5a7f6b060a7a8224705866c
MauricioLsalles <mauricio.leano@jala.university>
2025-07-31 08:43:51 -0400

    refactor: remove validation console log

--- src/domains/course/utils/Validations.js
+++ src/domains/course/utils/Validations.js
@@ -1,7 +1,6 @@
 export function isAvalidateUrl(url) {
   try {
     const validUrl = new URL(url);
-    console.log(validUrl.toString() !== "");
     return validUrl.toString() !== "";
   } catch {
     return false;
