f56c29fd1164703abbb9f03ff529923878ed99f9
Ariel Monroy <ariel.monroy@jala.university>
2025-07-31 15:47:35 -0400

    feat: refactor SearchRequestPage to use custom pagination and improve data fetching

--- src/domains/teacher/pages/SearchRequestPage.jsx
+++ src/domains/teacher/pages/SearchRequestPage.jsx
@@ -1,33 +1,45 @@
-import { useState, useEffect } from "react";
 import { Searcher } from "../../../shared/components/molecules/Searcher";
 import { Filters } from "../../core/componentes/organism/Filters";
 import { Pagination } from "../../../shared/components/molecules/Pagination";
 import RequestCard from "../components/molecules/RequestCard";
+import { usePagination } from "../../../shared/hooks/usePagination";
 
 export const SearchRequestPage = () => {
-  const [requests, setRequests] = useState([]);
   const activeFilters = ["category", "subcategory", "language"];
-  useEffect(() => {
-    fetch("/requestRequests.json")
-      .then((res) => res.json())
-      .then((data) => setRequests(data))
-      .catch((err) => console.error("Error loading requests:", err));
-  }, []);
 
-  const handleBackward = () => {};
-  const handleForward = () => {};
+  const {
+    data,
+    total,
+    page,
+    size,
+    query,
+    nextPage,
+    previousPage,
+    setFilters,
+    setSearchQuery,
+  } = usePagination({
+    url: "/requests/search",
+  });
+
+  const handleBackward = () => previousPage();
+  const handleForward = () => nextPage();
   const handleSelected = () => {};
-  const handleIndexedDB = () => {
-    return [1, 2, 3];
-  };
 
   return (
     <div className="p-12 flex flex-col gap-2 justify-center">
       <h1 className="text-3xl font-bold mb-6 text-center">Students requests</h1>
-      <Searcher placeholder="Search requests just for you" />
-      <Filters activeFilters={activeFilters} />
+      <Searcher
+        placeholder="Search requests just for you"
+        query={query}
+        setSearchQuery={setSearchQuery}
+      />
+      <Filters
+        total={total}
+        activeFilters={activeFilters}
+        setFilters={setFilters}
+      />
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
-        {requests.map((request) => (
+        {data.map((request) => (
           <RequestCard key={request.id} request={request} />
         ))}
       </div>
@@ -35,7 +47,9 @@ export const SearchRequestPage = () => {
         backwardFn={handleBackward}
         forwardFn={handleForward}
         selectedFn={handleSelected}
-        indexedDB={handleIndexedDB()}
+        currentPage={page}
+        size={size}
+        total={total}
       />
     </div>
   );
