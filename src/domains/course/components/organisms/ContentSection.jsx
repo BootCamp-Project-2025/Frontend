b131432aeba5f79fb966b058954c989ccee01d2a
MauricioLsalles <mauricio.leano@jala.university>
2025-07-30 22:10:46 -0400

    refactor post form

---
+++ src/domains/course/components/organisms/ContentSection.jsx
@@ -0,0 +1,40 @@
+import { Outlet } from "react-router-dom";
+import P2PNavbar from "../molecules/P2PNavbar";
+import PropTypes from "prop-types";
+
+export default function ContentSection({
+  postList,
+  filePostList,
+  edit,
+  erase,
+}) {
+  return (
+    <div className="px-4">
+      <P2PNavbar />
+      <Outlet context={{ postList, filePostList, erase, edit }} />
+    </div>
+  );
+}
+
+ContentSection.propTypes = {
+  postList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      creationDate: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+
+  filePostList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      creationDate: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+
+  edit: PropTypes.func,
+  erase: PropTypes.func,
+};
