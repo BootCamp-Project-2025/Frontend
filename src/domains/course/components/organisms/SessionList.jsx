1d44555dbc6f42653d21bd7f59cf6b4ba0567c46
MauricioLsalles <mauricio.leano@jala.university>
2025-08-04 10:53:27 -0400

    feat: created session list

---
+++ src/domains/course/components/organisms/SessionList.jsx
@@ -0,0 +1,108 @@
+import { useCallback, useMemo } from "react";
+import { Button } from "../../../../shared/components/atoms/Button";
+import { Title } from "../../../../shared/components/atoms/Title";
+import SessionCard from "./SessionCard";
+import PropTypes from "prop-types";
+
+export default function SessionList({
+  sessionList,
+  save,
+  edit,
+  erase,
+  complete,
+  remainingSession,
+}) {
+  const completedSessions = useMemo(
+    () => sessionList.filter((session) => session.status === "COMPLETED"),
+    [sessionList]
+  );
+  const availableSessions = useMemo(
+    () =>
+      sessionList.filter((session) => {
+        return (
+          (session.status === "PENDING") &
+          (new Date(session.dateOfTheSession).getTime() >= new Date().getTime())
+        );
+      }),
+    [sessionList]
+  );
+  const expiredSessions = useMemo(
+    () =>
+      sessionList.filter((session) => {
+        return (
+          (session.status === "PENDING") &
+          (new Date(session.dateOfTheSession).getTime() < new Date().getTime())
+        );
+      }),
+    [sessionList]
+  );
+
+  const handleCreate = useCallback(() => {
+    save("SESSION");
+  }, [save]);
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex">
+        <Title className="" size="xl" color="default">
+          Sessions
+        </Title>
+        <span className="my-auto ml-5">Pending: {remainingSession}</span>
+        <Button
+          onClick={handleCreate}
+          className="ml-auto"
+          radius="full"
+          variant="bordered"
+        >
+          +
+        </Button>
+      </div>
+      {sessionList.length === 0 ? (
+        <div className={`p2pCard-template`}>No content yet</div>
+      ) : (
+        <></>
+      )}
+      {availableSessions.map((session) => (
+        <SessionCard
+          className={"p2pCard-shadow-active"}
+          edit={edit}
+          erase={erase}
+          key={session.id}
+          session={session}
+        />
+      ))}
+      {expiredSessions.map((session) => (
+        <SessionCard
+          className={"p2pCard-shadow-disabled"}
+          complete={complete}
+          edit={edit}
+          erase={erase}
+          key={session.id}
+          session={session}
+        />
+      ))}
+      {completedSessions.map((session) => (
+        <SessionCard
+          className={"p2pCard-shadow-active"}
+          key={session.id}
+          session={session}
+        />
+      ))}
+    </div>
+  );
+}
+
+SessionList.propTypes = {
+  sessionList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      dateOfTheSession: PropTypes.instanceOf(Date).isRequired,
+      status: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  remainingSession: PropTypes.number.isRequired,
+  save: PropTypes.func,
+  edit: PropTypes.func,
+  erase: PropTypes.func,
+  complete: PropTypes.func,
+};
