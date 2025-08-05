ca3845261ceeeb2b953913bfb3417b8ec8c9d071
MauricioLsalles <mauricio.leano@jala.university>
2025-08-05 11:14:26 -0400

    fix: fixed date test

--- test/domains/course/components/molecules/SessionTime.test.jsx
+++ test/domains/course/components/molecules/SessionTime.test.jsx
@@ -2,15 +2,16 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import SessionTime from "../../../../../src/domains/course/components/molecules/SessionTime";
 
-const TIME = 1753905286346;
+const TIME = "2025-07-30T15:54:00";
 
+const date = new Date(TIME);
 describe("SessionTime molecule", () => {
   it("Shows time correctly", async () => {
     render(<SessionTime timestamp={TIME} />);
-    const date = await screen.findByText("30/7/2025");
-    const time = await screen.findByText("03:54 p.m.");
+    const dateText = await screen.findByText(date.toLocaleDateString());
+    const timeText = await screen.findByText("03:54 p.m.");
 
-    expect(date).toBeInTheDocument();
-    expect(time).toBeInTheDocument();
+    expect(dateText).toBeInTheDocument();
+    expect(timeText).toBeInTheDocument();
   });
 });
