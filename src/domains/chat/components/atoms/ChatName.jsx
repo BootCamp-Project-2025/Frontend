5254bf3f50c7548b866c99bebb76b90f62842f2d
cclaurevju <Carlos.Claure@jala.university>
2025-08-06 18:11:24 -0400

    add badge to chats

--- src/domains/chat/components/atoms/ChatName.jsx
+++ src/domains/chat/components/atoms/ChatName.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import { Title } from "../../../../shared/components/atoms/Title";
 import { useMemo } from "react";
+import Badge from "../../../../shared/components/atoms/Badge";
 export function ChatName({ chat, userName, showUserName }) {
   const showBadge = useMemo(() => {
     return chat.status != "ACTIVE";
@@ -11,9 +12,26 @@ export function ChatName({ chat, userName, showUserName }) {
     else return userName;
   }, [chat.name, userName, showUserName]);
 
+  const badgeColor = useMemo(() => {
+    switch (chat.status) {
+      case "PROPOSAL":
+        return "yellow";
+      case "P2P":
+        return "blue";
+      case "CLOSED":
+        return "red";
+      default:
+        return "gray";
+    }
+  }, [chat.status]);
+
   return (
     <div className="flex items-end gap-2 max-w-4/5 overflow-x-hidden">
-      {showBadge ? <p>{chat.status}</p> : null}
+      {showBadge ? (
+        <Badge color={badgeColor} className="mt-1">
+          {chat.status}
+        </Badge>
+      ) : null}
       <Title size="md" color="default" className="">
         {formatedName}
       </Title>
